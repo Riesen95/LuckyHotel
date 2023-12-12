440f31da22edeb5c5abcda0ce794ef98a47d4e30
Riesen95 <gumakow@gmail.com>
2023-12-12 18:17:29 +0100

    some frontend

--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -35,25 +35,38 @@ function Homescreen() {
   }, []);
 
   function filterByDate(dates) {
-    // const from = moment(dates[0].$d).format('DD-MM-YYYY');
-    // const to = moment(dates[1].$d).format('DD-MM-YYYY');
+    // Überprüft, ob 'dates' ein Array ist und mindestens zwei Elemente enthält
     if (Array.isArray(dates) && dates.length >= 2) {
+      // Setzt die Uhrzeit des Startdatums auf 12:00 Uhr mittags
       dates[0].$d.setHours(12, 0, 0, 0);
+      // Setzt die Uhrzeit des Enddatums auf 12:00 Uhr mittags
       dates[1].$d.setHours(12, 0, 0, 0);
+      // Konvertiert das Startdatum in einen Zeitstempel
       const selFrom = dates[0].$d.getTime();
+      // Konvertiert das Enddatum in einen Zeitstempel
       const selTo = dates[1].$d.getTime();
+      // Aktualisiert den State für das Startdatum
       setfromdate(dates[0].$d);
+      // Aktualisiert den State für das Enddatum
       settodate(dates[1].$d);
 
+      // Erstellt ein temporäres Array für verfügbare Zimmer
       const temprooms = [];
 
+      // Durchläuft alle Zimmer
       for (const room of rooms) {
+        // Startet mit der Annahme, dass das Zimmer verfügbar ist
         let availability = true;
+        // Überprüft, ob das Zimmer aktuelle Buchungen hat
         if (room.currentbookings.length > 0) {
+          // Durchläuft alle Buchungen des Zimmers
           for (const booking of room.currentbookings) {
+            // Konvertiert das Startdatum der Buchung in einen Zeitstempel
             const from = new Date(booking.fromdate).getTime();
+            // Konvertiert das Enddatum der Buchung in einen Zeitstempel
             const to = new Date(booking.todate).getTime();
 
+            // Überprüft, ob die Buchung mit dem ausgewählten Datum überschneidet
             if (to <= selFrom || from >= selTo) {
               availability = true;
             } else {
@@ -62,15 +75,19 @@ function Homescreen() {
             }
           }
         }
+        // Fügt das Zimmer zum temporären Array hinzu, wenn es verfügbar ist
         if (availability) {
           temprooms.push(room);
         }
       }
 
+      // Aktualisiert den State mit den gefilterten Zimmern
       setFilteredRooms(temprooms);
     } else {
+      // Setzt das Start- und Enddatum zurück, wenn die Eingabe ungültig ist
       setfromdate(null);
       settodate(null);
+      // Setzt die gefilterten Zimmer auf die Gesamtliste der Zimmer zurück
       setFilteredRooms([...rooms]);
     }
   }
@@ -116,7 +133,13 @@ function Homescreen() {
         </div>
 
         <div className="col-md-4">
-          <select className="form-control" value={Type}onChange={(e)=>{filterByType(e.target.value)}}>
+          <select
+            className="form-control"
+            value={Type}
+            onChange={(e) => {
+              filterByType(e.target.value);
+            }}
+          >
             <option value="Alle">Alle</option>
             <option value="Mit Balkon">Mit Balkon</option>
             <option value="Ohne Balkon">Ohne Balkon</option>
@@ -125,18 +148,18 @@ function Homescreen() {
       </div>
 
       <div className="row justify-content-center mt-5">
-  {loading ? (
-    <Loader />
-  ) : (
-    filteredRooms.map((room) => {
-      return (
-        <div key={room._id} className="col-md-9 mt-2">
-          <Room
-            room={room}
-            fromdate={fromdate}
-            todate={todate}
-            unavailable={!filteredRooms.find((r) => r._id === room._id)}
-          />
+        {loading ? (
+          <Loader />
+        ) : (
+          filteredRooms.map((room) => {
+            return (
+              <div key={room._id} className="col-md-9 mt-2">
+                <Room
+                  room={room}
+                  fromdate={fromdate}
+                  todate={todate}
+                  unavailable={!filteredRooms.find((r) => r._id === room._id)}
+                />
               </div>
             );
           })
